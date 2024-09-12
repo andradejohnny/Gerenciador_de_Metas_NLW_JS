6eb6bd20abb803080274ec977202bb8037a44da3
Johnny Andrade <johnnywandrade@gmail.com>
2024-09-11 21:36:54 -0300

    Cadastro de meta

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
-const start = () => {
+const { select, input } = require('@inquirer/prompts');
+
+let meta = {
+  value: 'Tomar 3l de água por dia',
+  checked: false,
+};
+let metas = [meta];
+
+const cadastrarMeta = async () => {
+  const meta = await input({ meessage: 'Digite a meta: ' });
+
+  if (meta.length == 0) {
+    console.log('A meta não pode ser vazia!');
+    return;
+  }
+
+  metas.push({ value: meta, checked: false });
+};
+
+const start = async () => {
   let count = 0;
+
   while (true) {
-    let opcao = 'sair';
+    const opcao = await select({
+      message: 'Menu >',
+      choices: [
+        {
+          name: 'Cadastrar meta',
+          value: 'cadastrar',
+        },
+        {
+          name: 'Listar metas',
+          value: 'listar',
+        },
+        {
+          name: 'Sair',
+          value: 'sair',
+        },
+      ],
+    }); //faz o while aguardar o usuario digitar algo ao inves de ficar rodando o loop
+
     switch (opcao) {
       case 'cadastrar':
+        await cadastrarMeta();
+        console.log(metas);
         console.log('Vamos cadastrar');
         break;
       case 'listar':
